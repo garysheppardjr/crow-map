32881dc68eef085e7b5c2d869c4220a7be70bfef
Gary Sheppard <gsheppard@esri.com>
2015-12-22 16:07:25 -0500

    Added FileUtilities for Android

---
+++ source/crow-map-android/app/src/main/java/com/esri/defensese/crowmap/android/model/FileUtilities.java
@@ -0,0 +1,67 @@
+/*******************************************************************************
+ * Copyright 2015 Esri
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ ******************************************************************************/
+package com.esri.defensese.crowmap.android.model;
+
+import java.io.InputStream;
+import java.util.HashSet;
+import java.util.Locale;
+
+/**
+ * Utilities for accessing the filesystem.
+ */
+public class FileUtilities {
+
+    /**
+     * Returns file paths for external mounts, excluding /mnt/sdcard (which is internal). This code
+     * is adapted from http://stackoverflow.com/a/13648873/720773 .
+     * @return file paths for external mounts.
+     */
+    public static HashSet<String> getExternalMounts() {
+        final HashSet<String> out = new HashSet<String>();
+        String reg = "(?i).*vold.*(vfat|ntfs|exfat|fat32|ext3|ext4).*rw.*";
+        String s = "";
+        try {
+            final Process process = new ProcessBuilder().command("mount")
+                    .redirectErrorStream(true).start();
+            process.waitFor();
+            final InputStream is = process.getInputStream();
+            final byte[] buffer = new byte[1024];
+            while (is.read(buffer) != -1) {
+                s = s + new String(buffer);
+            }
+            is.close();
+        } catch (final Exception e) {
+            e.printStackTrace();
+        }
+
+        // parse output
+        final String[] lines = s.split("\n");
+        for (String line : lines) {
+            if (!line.toLowerCase(Locale.US).contains("asec")) {
+                if (line.matches(reg)) {
+                    String[] parts = line.split(" ");
+                    for (String part : parts) {
+                        if (part.startsWith("/"))
+                            if (!part.toLowerCase(Locale.US).contains("vold"))
+                                out.add(part);
+                    }
+                }
+            }
+        }
+        return out;
+    }
+
+}
